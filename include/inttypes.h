7ab35236a6aa6c906ed14c6dbd71b37f07dfa1d1
Pierre Pronchery <khorben@defora.org>
2005-02-15 20:54:03 +0000

    Initial release

---
+++ include/inttypes.h
@@ -0,0 +1,10 @@
+/* inttypes.h */
+
+
+
+#ifndef ___INTTYPES_H
+# define ___INTTYPES_H
+
+# include <stdint.h>
+
+#endif /* !___INTTYPES_H */
---
+++ include/stdint.h
@@ -0,0 +1,16 @@
+/* stdint.h */
+
+
+
+#ifndef ___STDINT_H
+# define ___STDINT_H
+
+
+typedef char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#endif /* !___STDINT_H */
