b6cd09a54224cb13ab2f767a3d415fb00d24b600
Pierre Pronchery <khorben@defora.org>
2005-02-17 04:48:07 +0000

    Initial release

---
+++ include/sys/stat.h
@@ -0,0 +1,38 @@
+/* sys/stat.h */
+
+
+
+#ifndef ___SYS_STAT_H
+# define ___SYS_STAT_H
+
+
+/* types */
+struct stat {
+	dev_t st_dev;
+	ino_t st_ino;
+	mode_t st_mode;
+	nlink_t st_nlink;
+	uid_t st_uid;
+	gid_t st_gid;
+	dev_t st_rdev;
+	off_t st_size;
+	time_t st_atime;
+	time_t st_mtime;
+	time_t st_ctime;
+	blksize_t st_blksize;
+	blkcnt_t st_blocks;
+};
+
+
+/* functions */
+int chmod(const char *, mode_t);
+int fchmod(int, mode_t);
+int fstat(int, struct stat *);
+int lstat(const char *, struct stat *);
+int mkdir(const char *, mode_t);
+int mkfifo(const char *, mode_t);
+int mknod(const char *, mode_t, dev_t);
+int stat(const char *, struct stat *);
+mode_t umask(mode_t);
+
+#endif /* !___SYS_STAT_H */
