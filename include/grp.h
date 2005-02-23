048432bda33cd03bc76234a73c3fb8b55f7bf610
Pierre Pronchery <khorben@defora.org>
2005-02-23 01:04:47 +0000

    Added essential types and functions

--- include/grp.h
+++ include/grp.h
@@ -5,4 +5,26 @@
 #ifndef ___GRP_H
 # define ___GRP_H
 
+
+/* types */
+# ifndef id_t
+#  define id_t id_t
+typedef int id_t;
+# endif
+# ifndef gid_t
+#  define gid_t gid_t
+typedef id_t gid_t
+# endif
+struct group
+{
+	char * gr_name;
+	gid_t gr_gid;
+	char ** gr_mem;
+};
+
+
+/* functions */
+struct group * getgrgid(gid_t);
+struct group * getgrnam(char const *);
+
 #endif /* !___GRP_H */
