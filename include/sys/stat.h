d31c2f756e02b13e812256c074d28ff62fb79d74
Pierre Pronchery <khorben@defora.org>
2005-02-23 01:35:06 +0000

    Added necessary types

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -7,17 +7,50 @@
 
 
 /* types */
+# ifndef blkcnt_t
+#  define blkcnt_t blkcnt_t
+typedef int blkcnt_t;
+# endif
+# ifndef blksize_t
+#  define blksize_t blksize_t
+typedef int blksize_t;
+# endif
+# ifndef dev_t
+#  define dev_t dev_t
+typedef int dev_t;
+# endif
+# ifndef id_t
+#  define id_t id_t
+typedef int id_t;
+# endif
+# ifndef ino_t
+#  define ino_t ino_t
+typedef int ino_t;
+# endif
 # ifndef mode_t
 #  define mode_t mode_t
 typedef int mode_t;
 # endif
+# ifndef nlink_t
+#  define nlink_t nlink_t
+typedef int nlink_t;
+# endif
+# ifndef off_t
+#  define off_t off_t
+typedef int off_t;
+# endif
+# ifndef time_t
+#  define time_t time_t
+typedef int time_t;
+# endif
+
 # ifndef gid_t
 #  define gid_t gid_t
-typedef int gid_t;
+typedef id_t gid_t;
 # endif
 # ifndef uid_t
 #  define uid_t uid_t
-typedef int uid_t;
+typedef id_t uid_t;
 # endif
 struct stat {
 	dev_t st_dev;
@@ -37,14 +70,14 @@ struct stat {
 
 
 /* functions */
-int chmod(const char *, mode_t);
+int chmod(char const *, mode_t);
 int fchmod(int, mode_t);
 int fstat(int, struct stat *);
-int lstat(const char *, struct stat *);
-int mkdir(const char *, mode_t);
-int mkfifo(const char *, mode_t);
-int mknod(const char *, mode_t, dev_t);
-int stat(const char *, struct stat *);
+int lstat(char const *, struct stat *);
+int mkdir(char const *, mode_t);
+int mkfifo(char const *, mode_t);
+int mknod(char const *, mode_t, dev_t);
+int stat(char const *, struct stat *);
 mode_t umask(mode_t);
 
 #endif /* !___SYS_STAT_H */
