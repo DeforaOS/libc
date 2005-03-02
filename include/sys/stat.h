55f99ffd81fb78336db0b7d4f6b70831dd9bacbb
Pierre Pronchery <khorben@defora.org>
2005-03-02 19:06:12 +0000

    Added more file mode constants

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -78,6 +78,18 @@ struct stat {
 # define S_IFDIR 0040000
 # define S_IFLNK 0120000
 # define S_IFSOCK 0140000
+# define S_IRUSR 0400
+# define S_IWUSR 0200
+# define S_IXUSR 0100
+# define S_IRWXU (S_IRUSR | S_IWUSR | S_IXUSR)
+# define S_IRGRP 040
+# define S_IWGRP 020
+# define S_IXGRP 010
+# define S_IRWXG (S_IRGRP | S_IWGRP | S_IXGRP)
+# define S_IROTH 04
+# define S_IWOTH 02
+# define S_IXOTH 01
+# define S_IRWXO (S_IROTH | S_IWOTH | S_IXOTH)
 
 
 /* macros */
