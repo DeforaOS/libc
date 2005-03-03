980256feb840d6a547c91b938a32d436f9592d40
Pierre Pronchery <khorben@defora.org>
2005-03-03 12:16:18 +0000

    Initial release

---
+++ include/stdarg.h
@@ -0,0 +1,21 @@
+/* stdarg.h */
+
+
+
+#ifndef ___STDARG_H
+# define ___STDARG_H
+
+
+/* types */
+# ifndef va_list
+#  define va_list va_list
+typedef void * va_list;
+# endif
+
+
+/* macros */
+# define va_start(ap, arg) (ap) = (arg)
+# define va_arg(ap, type) ((ap) += sizeof(type))
+# define va_end(ap)
+
+#endif /* !___STDARG_H */
