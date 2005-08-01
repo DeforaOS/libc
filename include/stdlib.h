aeb5a64de8aa86ff8351e165ec675fd66d979245
Pierre Pronchery <khorben@defora.org>
2005-08-01 20:59:26 +0000

    Added getenv(), setenv(), unsetenv() prototypes

--- include/stdlib.h
+++ include/stdlib.h
@@ -15,8 +15,11 @@ long atol(char const * str);
 long long atoll(char const * nptr);
 void exit(int status);
 void free(void * ptr);
+char * getenv(char const * name);
 void * malloc(size_t size);
 void * realloc(void * ptr, size_t size);
+int setenv(char const * name, char const * value, int overwrite);
 long strtol(char const * str, char ** endptr, int base);
+int unsetenv(char const * name);
 
 #endif /* !___STDLIB_H */
