8e9df05f22296b541bc305bd4aefd52840f6719a
Pierre Pronchery <khorben@defora.org>
2007-01-07 21:50:14 +0000

    Implemented time()

--- src/time.c
+++ src/time.c
@@ -3,11 +3,20 @@
 
 
 
+#include "sys/time.h"
+#include "stdlib.h"
 #include "syscalls.h"
 #include "time.h"
 
 
 /* time */
-#ifndef __NetBSD__
-syscall1(time_t, time, time_t *, t);
-#endif
+time_t time(time_t * t)
+{
+	struct timeval tv;
+
+	if(gettimeofday(&tv, NULL) != 0)
+		return -1;
+	if(t != NULL)
+		*t = tv.tv_sec;
+	return tv.tv_sec;
+}
