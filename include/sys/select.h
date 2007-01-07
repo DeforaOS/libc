7f2114107541f987c6133a59915b72b7a4d0296d
Pierre Pronchery <khorben@defora.org>
2007-01-07 21:58:20 +0000

    Initial release

---
+++ include/sys/select.h
@@ -0,0 +1,43 @@
+/* $Id$ */
+/* Copyright (c) 2006 The DeforaOS Project */
+
+
+
+#ifndef LIBC_SYS_SELECT_H
+# define LIBC_SYS_SELECT_H
+
+
+/* types */
+# ifndef fd_set
+#  define fd_set fd_set
+typedef struct _fd_set fd_set;
+# endif
+# ifndef suseconds_t
+#  define suseconds_t suseconds_t
+typedef int suseconds_t;
+# endif
+# ifndef time_t
+#  define time_t time_t
+typedef int time_t;
+# endif
+# ifndef timeval
+#  define timeval timeval
+struct timeval
+{
+	time_t tv_sec;
+	suseconds_t tv_usec;
+};
+# endif
+
+struct itimerval
+{
+	struct timeval it_interval;
+	struct timeval it_value;
+};
+
+
+/* functions */
+int select(int fdcnt, fd_set * rfds, fd_set * wfds, fd_set * efds,
+		struct timeval * timeout);
+
+#endif /* !LIBC_SYS_SELECT_H */
---
+++ include/sys/time.h
@@ -0,0 +1,44 @@
+/* $Id$ */
+/* Copyright (c) 2006 The DeforaOS Project */
+
+
+
+#ifndef LIBC_SYS_TIME_H
+# define LIBC_SYS_TIME_H
+
+
+/* types */
+# ifndef fd_set
+#  define fd_set fd_set
+typedef struct _fd_set fd_set;
+# endif
+# ifndef suseconds_t
+#  define suseconds_t suseconds_t
+typedef int suseconds_t;
+# endif
+# ifndef time_t
+#  define time_t time_t
+typedef int time_t;
+# endif
+#ifndef timeval
+# define timeval timeval
+struct timeval
+{
+	time_t tv_sec;
+	suseconds_t tv_usec;
+};
+#endif
+
+struct itimerval
+{
+	struct timeval it_interval;
+	struct timeval it_value;
+};
+
+
+/* functions */
+int gettimeofday(struct timeval * tv, void * null);
+int select(int fdcnt, fd_set * rfds, fd_set * wfds, fd_set * efds,
+		struct timeval * timeout);
+
+#endif /* !LIBC_SYS_TIME_H */
