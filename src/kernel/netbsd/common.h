bc823051aafd074c3a324d03cabbdb6566b27454
Pierre Pronchery <khorben@defora.org>
2007-01-10 15:41:55 +0000

    Adding list of syscalls

--- src/kernel/netbsd/common.h
+++ src/kernel/netbsd/common.h
@@ -7,6 +7,19 @@
 # define KERNEL_NETBSD_COMMON_H
 
 
-/* FIXME add list of syscalls */
+# define SYS_open		5
+# define SYS_wait4		7
+# define SYS_chmod		15
+# define SYS_kill		37
+# define SYS_umask		60
+# define SYS_munmap		73
+# define SYS_gettimeofday	116
+# define SYS_mkdir		136
+# define SYS_mmap		197
+# define SYS_nanosleep		240
+# define SYS_stat		278
+# define SYS_fstat		279
+# define SYS_lstat		280
+# define SYS_statvfs		357
 
 #endif /* !KERNEL_NETBSD_COMMON_H */
