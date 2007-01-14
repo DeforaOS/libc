6a0083476f5d184e942dd217001ae334005f5199
Pierre Pronchery <khorben@defora.org>
2007-01-14 02:51:46 +0000

    Added kernel compatibility interface for errno

---
+++ include/kernel/linux/errno.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+/* Copyright (c) 2007 The DeforaOS Project */
+
+
+
+#ifndef LIBC_KERNEL_LINUX_ERRNO_H
+# define LIBC_KERNEL_LINUX_ERRNO_H
+
+
+/* constants */
+# define E2BIG		7
+# define EBADF		9
+# define EACCES		13
+# define EXDEV		18
+# define ENOTDIR	20
+# define EINVAL		22
+# define ERANGE		34
+# define ENOSYS		38
+# define ENOBUFS	105
+
+#endif /* !LIBC_KERNEL_LINUX_ERRNO_H */
---
+++ include/kernel/netbsd/errno.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+/* Copyright (c) 2007 The DeforaOS Project */
+
+
+
+#ifndef LIBC_KERNEL_NETBSD_ERRNO_H
+# define LIBC_KERNEL_NETBSD_ERRNO_H
+
+
+/* constants */
+# define E2BIG		7
+# define EBADF		9
+# define EACCES		13
+# define EXDEV		18
+# define ENOTDIR	20
+# define EINVAL		22
+# define ERANGE		34
+# define ENOBUFS	55
+# define ENOSYS		78
+
+#endif /* !LIBC_KERNEL_NETBSD_ERRNO_H */
