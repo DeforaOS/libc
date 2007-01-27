41080f030627b96a34e88cf8d77ceebd2ea5b43e
Pierre Pronchery <khorben@defora.org>
2007-01-27 00:20:43 +0000

    Was using wrong values

--- include/kernel/netbsd/sys/mman.h
+++ include/kernel/netbsd/sys/mman.h
@@ -13,9 +13,9 @@
 # define PROT_EXEC	0x4
 # define PROT_NONE	0x0
 
-# define MAP_SHARED	0x01
-# define MAP_PRIVATE	0x02
-# define MAP_FIXED	0x10
-# define MAP_ANONYMOUS	0x20
+# define MAP_SHARED	0x0001
+# define MAP_PRIVATE	0x0002
+# define MAP_FIXED	0x0010
+# define MAP_ANONYMOUS	0x1000
 
 #endif /* !LIBC_KERNEL_NETBSD_SYS_MMAN_H */
