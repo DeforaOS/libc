8bcce2e46fe53e27bf4a9a8b997ded4ca9ded94b
Pierre Pronchery <khorben@defora.org>
2007-01-30 19:11:38 +0000

    Added more errno constants

--- include/kernel/linux/errno.h
+++ include/kernel/linux/errno.h
@@ -8,15 +8,27 @@
 
 
 /* constants */
+# define EPERM		1
+# define ENOENT		2
+# define EINTR		4
+# define EIO		5
 # define E2BIG		7
 # define ENOEXEC	8
 # define EBADF		9
 # define ECHILD		10
+# define EAGAIN		11
 # define ENOMEM		12
 # define EACCES		13
+# define EFAULT		14
+# define EBUSY		16
+# define EEXIST		17
 # define EXDEV		18
 # define ENOTDIR	20
+# define EISDIR		21
 # define EINVAL		22
+# define ENOTTY		25
+# define EROFS		30
+# define EPIPE		32
 # define ERANGE		34
 # define ENOSYS		38
 # define ENOBUFS	105
