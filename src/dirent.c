bc43797041c5af1e21c09ea4edfcf3e2e7526c83
Pierre Pronchery <khorben@defora.org>
2007-02-03 19:59:58 +0000

    Fixed call to open() in opendir()

--- src/dirent.c
+++ src/dirent.c
@@ -43,7 +43,7 @@ DIR * opendir(char const * name)
 	int fd;
 	struct stat st;
 
-	if((fd = open(name, O_RDONLY)) != 0)
+	if((fd = open(name, O_RDONLY)) == -1)
 		return NULL;
 	if(fcntl(fd, F_SETFD, FD_CLOEXEC) != 0 || fstat(fd, &st) != 0)
 	{
