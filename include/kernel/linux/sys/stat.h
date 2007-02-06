cf6fa38e6e09cfd514f0d76cd76804d6b5150786
Pierre Pronchery <khorben@defora.org>
2007-02-06 00:38:29 +0000

    Added missing types

--- include/kernel/linux/sys/stat.h
+++ include/kernel/linux/sys/stat.h
@@ -8,6 +8,52 @@
 
 
 /* types */
+# ifndef blkcnt_t
+#  define blkcnt_t blkcnt_t
+typedef unsigned int blkcnt_t;
+# endif
+# ifndef blksize_t
+#  define blksize_t blksize_t
+typedef int blksize_t;
+# endif
+# ifndef dev_t
+#  define dev_t dev_t
+typedef unsigned long long dev_t;
+#endif
+# ifndef id_t
+#  define id_t id_t
+typedef int id_t;
+# endif
+# ifndef ino_t
+#  define ino_t ino_t
+typedef int ino_t;
+# endif
+# ifndef mode_t
+#  define mode_t mode_t
+typedef int mode_t;
+# endif
+# ifndef nlink_t
+#  define nlink_t nlink_t
+typedef int nlink_t;
+# endif
+# ifndef off_t
+#  define off_t off_t
+typedef int off_t;
+# endif
+# ifndef time_t
+#  define time_t time_t
+typedef unsigned int time_t;
+# endif
+
+# ifndef gid_t
+#  define gid_t gid_t
+typedef id_t gid_t;
+# endif
+# ifndef uid_t
+#  define uid_t uid_t
+typedef id_t uid_t;
+# endif
+
 struct stat
 {
 	dev_t st_dev;
