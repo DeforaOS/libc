e12f86c9d4a271f442afde589455ea90a88c7311
Pierre Pronchery <khorben@defora.org>
2007-06-02 15:54:46 +0000

    Implemented endutxent() and getutxent() (not functional)

--- src/utmpx.c
+++ src/utmpx.c
@@ -3,4 +3,49 @@
 
 
 
+#include "stdio.h"
 #include "utmpx.h"
+
+
+/* private */
+static FILE * _fp = NULL;
+
+
+/* public */
+/* endutxent */
+void endutxent(void)
+{
+	if(_fp == NULL)
+		return;
+	fclose(_fp);
+	_fp = NULL;
+}
+
+
+/* getutxent */
+struct utmpx * getutxent(void)
+{
+	static struct utmpx ret;
+
+	if(_fp == NULL && (_fp = fopen("/var/run/utmpx", "r")) == NULL)
+		return NULL;
+	if(fread(&ret, 1, sizeof(ret), _fp) != 1)
+	{
+		fclose(_fp);
+		_fp = NULL;
+		return NULL;
+	}
+	return &ret;
+}
+
+
+/* getutxline */
+/* FIXME implement */
+
+
+/* pututxline */
+/* FIXME implement */
+
+
+/* setutxent */
+/* FIXME implement */
