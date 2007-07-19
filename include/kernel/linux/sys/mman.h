a376d1dfde11f1c846aeecb090262ceec481c053
Pierre Pronchery <khorben@defora.org>
2007-07-19 14:26:43 +0000

    Code cleanup

--- include/kernel/linux/sys/mman.h
+++ include/kernel/linux/sys/mman.h
@@ -28,10 +28,10 @@ typedef long long off_t;
 
 
 /* constants */
+# define PROT_NONE	0x0
 # define PROT_READ	0x1
 # define PROT_WRITE	0x2
 # define PROT_EXEC	0x4
-# define PROT_NONE	0x0
 
 # define MAP_SHARED	0x01
 # define MAP_PRIVATE	0x02
