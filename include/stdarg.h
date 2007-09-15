8f8877898c8a52c9495d83438d5e4ae8d9b59b77
Pierre Pronchery <khorben@defora.org>
2007-09-15 23:54:12 +0000

    Added va_copy although _not_ tested

--- include/stdarg.h
+++ include/stdarg.h
@@ -26,6 +26,7 @@ typedef void * va_list;
 #  define va_start(ap, arg)	(ap) = ((char*)&arg) + 4
 #  define va_arg(ap, type)	((ap) += sizeof(type), \
 		*(type*)((void*)ap - sizeof(type)))
+#  define va_copy(copy, arg)	(copy) = (arg)	/* FIXME not tested */
 #  define va_end(ap)
 # elif defined(__amd64__) \
 	|| defined(__arm__) \
@@ -35,12 +36,14 @@ typedef void * va_list;
 #  define va_list		__builtin_va_list
 #  define va_start(ap, arg)	__builtin_va_start((ap), (arg))
 #  define va_arg		__builtin_va_arg
+#  define va_copy		__builtin_va_copy
 #  define va_end(ap)		__builtin_va_end(ap)
 # else /* !__i386__ */
 #  warning Unsupported architecture
 typedef void * va_list;
 #  define va_start(ap, arg)
 #  define va_arg(ap, type) (type)(ap)
+#  define va_copy(dest, src)
 #  define va_end(ap)
 # endif
 
