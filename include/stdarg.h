6d1aa5d2b81bbf0abf4f39999042577dd4c31073
Pierre Pronchery <khorben@defora.org>
2007-09-15 23:52:14 +0000

    Should work on amd64 too (may have to use __x86_64__ instead)

--- include/stdarg.h
+++ include/stdarg.h
@@ -20,28 +20,25 @@
 # define LIBC_STDARG_H
 
 
-/* types */
-# ifndef va_list
-#  define va_list va_list
-typedef void * va_list;
-# endif
-
-
 /* macros */
 # if defined(__i386__)
+typedef void * va_list;
 #  define va_start(ap, arg)	(ap) = ((char*)&arg) + 4
 #  define va_arg(ap, type)	((ap) += sizeof(type), \
 		*(type*)((void*)ap - sizeof(type)))
 #  define va_end(ap)
-# elif defined(__arm__) \
+# elif defined(__amd64__) \
+	|| defined(__arm__) \
 	|| defined(__mips__) \
-	|| defined(__sparc64__) \
-	|| defined(__sparc__)	/* XXX compiler dependent */
-#  define va_start(ap, arg)	__builtin_va_start(ap, arg)
-#  define va_arg(ap, type)	__builtin_va_arg(ap, type)
+	|| defined(__sparc__) \
+	|| defined(__sparc64__)	/* XXX compiler dependent */
+#  define va_list		__builtin_va_list
+#  define va_start(ap, arg)	__builtin_va_start((ap), (arg))
+#  define va_arg		__builtin_va_arg
 #  define va_end(ap)		__builtin_va_end(ap)
 # else /* !__i386__ */
 #  warning Unsupported architecture
+typedef void * va_list;
 #  define va_start(ap, arg)
 #  define va_arg(ap, type) (type)(ap)
 #  define va_end(ap)
