1d58992b98691cfa85ecfb53233f1837891f49af
Pierre Pronchery <khorben@defora.org>
2007-09-16 09:36:01 +0000

    Fixed S_IS*() macros

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -49,13 +49,13 @@
 
 
 /* macros */
-# define S_ISBLK(m)	(((m) & S_IFBLK) == S_IFBLK)
-# define S_ISCHR(m)	(((m) & S_IFCHR) == S_IFCHR)
-# define S_ISDIR(m)	(((m) & S_IFDIR) == S_IFDIR)
-# define S_ISFIFO(m)	(((m) & S_IFIFO) == S_IFIFO)
-# define S_ISREG(m)	(((m) & S_IFREG) == S_IFREG)
-# define S_ISLNK(m)	(((m) & S_IFLNK) == S_IFLNK)
-# define S_ISSOCK(m)	(((m) & S_IFSOCK) == S_IFSOCK)
+# define S_ISBLK(m)	(((m) & S_IFMT) == S_IFBLK)
+# define S_ISCHR(m)	(((m) & S_IFMT) == S_IFCHR)
+# define S_ISDIR(m)	(((m) & S_IFMT) == S_IFDIR)
+# define S_ISFIFO(m)	(((m) & S_IFMT) == S_IFIFO)
+# define S_ISREG(m)	(((m) & S_IFMT) == S_IFREG)
+# define S_ISLNK(m)	(((m) & S_IFMT) == S_IFLNK)
+# define S_ISSOCK(m)	(((m) & S_IFMT) == S_IFSOCK)
 
 
 /* functions */
