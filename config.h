5f5b4284d34b456302418d926976ae5374dc5e6b
Pierre Pronchery <khorben@defora.org>
2007-09-26 14:20:42 +0000

    Initial release

---
+++ config.h
@@ -0,0 +1,6 @@
+/* $Id$ */
+/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System libc */
+
+/* uncomment this to build a shared library */
+/* #define WITH_SHARED 1 */
