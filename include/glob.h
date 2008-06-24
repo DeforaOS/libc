02e0534d89c6ee856ac604870a6c5684eee03ddc
Pierre Pronchery <khorben@defora.org>
2008-06-24 11:40:04 +0000

    Added stub for glob.h

---
+++ include/glob.h
@@ -0,0 +1,33 @@
+/* $Id$ */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System libc */
+/* libc is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * libc is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with libc; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+
+
+
+#ifndef LIBC_GLOB_H
+# define LIBC_GLOB_H
+
+
+/* types */
+# ifndef glob_t
+#  define glob_t glob_t
+typedef struct _glob_t
+{
+	size_t gl_pathc;
+	char ** gl_pathv;
+	size_t gl_offs;
+} glob_t;
+
+#endif /* !LIBC_GLOB_H */
