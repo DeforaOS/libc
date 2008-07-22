77608bec39d84c8ecfc2cd00d31230fa22f79980
Pierre Pronchery <khorben@defora.org>
2008-07-22 20:58:02 +0000

    Added LC_* constants, localeconv() and setlocale() functions

--- include/locale.h
+++ include/locale.h
@@ -58,4 +58,17 @@ struct lconv
 #  define NULL ((void*)0)
 # endif
 
+# define LC_ALL		0
+# define LC_COLLATE	1
+# define LC_CTYPE	2
+# define LC_MESSAGES	3
+# define LC_MONETARY	4
+# define LC_NUMERIC	5
+# define LC_TIME	6
+
+
+/* functions */
+struct lconv * localeconv(void);
+char * setlocale(int category, const char * locale);
+
 #endif /* !LIBC_LOCALE_H */
