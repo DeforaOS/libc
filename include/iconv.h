32c1981329cc5b7aad296ed78b94b2546cb3090f
Pierre Pronchery <khorben@defora.org>
2008-09-12 14:10:03 +0000

    Added the function prototypes

--- include/iconv.h
+++ include/iconv.h
@@ -21,7 +21,13 @@
 
 
 /* types */
-/* FIXME implement */
 typedef struct _iconv_t * iconv_t;
+
+
+/* functions */
+iconv_t iconv_open(const char * tocode, const char * fromcode);
+size_t iconv(iconv_t cd, char ** inbuf, size_t * inbytesleft,
+		char ** outbuf, size_t * outbytesleft);
+int iconv_close(iconv_t cd);
 
 #endif /* !LIBC_ICONV_H */
