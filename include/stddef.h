457c8d2834e483a359b7e6b67ea63acb31edc569
Pierre Pronchery <khorben@defora.org>
2008-10-16 19:33:35 +0000

    Added offsetof() macro

--- include/stddef.h
+++ include/stddef.h
@@ -40,4 +40,10 @@ typedef char wchar_t;
 #  define NULL ((void*)0)
 # endif
 
+
+/* macros */
+# ifndef offsetof
+#  define offsetof(type, member) ((size_t)&((type*)0)->member)
+# endif
+
 #endif /* !LIBC_STDDEF_H */
