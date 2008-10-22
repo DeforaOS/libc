4d5fb85f6d067252b4a6948c79620253b18c8c07
Pierre Pronchery <khorben@defora.org>
2008-10-22 14:40:05 +0000

    Inherit definitions from <sys/select.h>

--- include/sys/time.h
+++ include/sys/time.h
@@ -19,14 +19,11 @@
 #ifndef LIBC_SYS_TIME_H
 # define LIBC_SYS_TIME_H
 
+# include <sys/select.h>
 # include "compat/sys/time.h"
 
 
 /* types */
-# ifndef fd_set
-#  define fd_set fd_set
-typedef struct _fd_set fd_set;
-# endif
 # ifndef itimerval
 #  define itimerval itimerval
 struct itimerval
@@ -39,8 +36,6 @@ struct itimerval
 
 /* functions */
 int gettimeofday(struct timeval * tv, void * null);
-int select(int fdcnt, fd_set * rfds, fd_set * wfds, fd_set * efds,
-		struct timeval * timeout);
 int utimes(char const * path, const struct timeval times[2]);
 
 #endif /* !LIBC_SYS_TIME_H */
