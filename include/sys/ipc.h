942da347ded47f88aac66b1251ac963017ee9181
Pierre Pronchery <khorben@defora.org>
2008-10-28 12:00:09 +0000

    Initial release

---
+++ include/sys/ipc.h
@@ -0,0 +1,53 @@
+/* $Id$ */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System libc */
+/* libc is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * libc is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with libc; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+
+
+
+#ifndef LIBC_SYS_IPC_H
+# define LIBC_SYS_IPC_H
+
+
+/* types */
+# ifndef id_t
+#  define id_t id_t
+typedef unsigned int id_t;
+# endif
+# ifndef key_t
+#  define key_t key_t
+typedef long key_t;
+# endif
+# ifndef mode_t
+#  define mode_t mode_t
+typedef unsigned int mode_t;
+# endif
+
+# ifndef gid_t
+#  define gid_t gid_t
+typedef id_t gid_t;
+# endif
+# ifndef uid_t
+#  define uid_t uid_t
+typedef id_t uid_t;
+# endif
+
+
+/* constants */
+# define IPC_PRIVATE	((key_t)0)
+# define IPC_CREAT	01000
+# define IPC_EXCL	02000
+# define IPC_NOWAIT	04000
+
+#endif /* !LIBC_SYS_IPC_H */
