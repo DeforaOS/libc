111eb80c6c23234c111c35aa53216a716ea8b0fd
Pierre Pronchery <khorben@defora.org>
2008-10-28 16:38:14 +0000

    Checking for _statfs() instead of statfs()

--- src/sys/statvfs.c
+++ src/sys/statvfs.c
@@ -1,29 +1,38 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
-/* libc is not free software; you can redistribute it and/or modify it under
- * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
- * Unported as published by the Creative Commons organization.
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
  *
- * libc is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
- * ShareAlike 3.0 Unported license for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Creative Commons Attribution-
- * NonCommercial-ShareAlike 3.0 along with libc; if not, browse to
- * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 
 
 #include "string.h"
+#include "errno.h"
 #include "../syscalls.h"
 #include "sys/statvfs.h"
 
 
+/* fstatvfs */
+int fstatvfs(int fd, struct statvfs * buf)
+{
+	/* FIXME implement */
+	errno = ENOSYS;
+	return -1;
+}
+
+
 /* statvfs */
 #if !defined(SYS_statvfs)
-# if defined(SYS_statfs)
+# if defined(SYS__statfs)
 typedef long fsid_t;
 struct statfs
 {
@@ -37,12 +46,12 @@ struct statfs
 	fsid_t f_fsid;
 	long f_namelen;
 };
-int statfs(char const * path, struct statfs * buf);
+int _statfs(char const * path, struct statfs * buf);
 int statvfs(char const * path, struct statvfs * buf)
 {
 	struct statfs fs;
 
-	if(statfs(path, &fs) != 0)
+	if(_statfs(path, &fs) != 0)
 		return -1;
 	memset(buf, 0, sizeof(*buf));
 	buf->f_bsize = fs.f_bsize;
@@ -55,7 +64,12 @@ int statvfs(char const * path, struct statvfs * buf)
 	buf->f_namemax = fs.f_namelen;
 	return 0;
 }
-# else /* !SYS_statvfs && !SYS_statfs */
+# else /* !SYS_statvfs && !SYS__statfs */
 #  warning Unsupported platform: statvfs() is missing
+int statvfs(char const * path, struct statvfs * buf)
+{
+	errno = ENOSYS;
+	return -1;
+}
 # endif
 #endif
