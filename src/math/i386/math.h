ea6df36a7c99f2c60114f1aa776d4e5b824eb2c2
Pierre Pronchery <khorben@defora.org>
2008-11-02 02:16:22 +0000

    Initial release

---
+++ src/math/i386/math.h
@@ -0,0 +1,37 @@
+/* $Id$ */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
+/* This file is part of DeforaOS System libc */
+/* libc is not free software; you can redistribute it and/or modify it under
+ * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
+ * Unported as published by the Creative Commons organization.
+ *
+ * libc is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
+ * ShareAlike 3.0 Unported license for more details.
+ *
+ * You should have received a copy of the Creative Commons Attribution-
+ * NonCommercial-ShareAlike 3.0 along with libc; if not, browse to
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+
+
+
+#ifndef MATH_I386_H
+# define MATH_I386_H
+
+
+#define ARCH_ceil
+#define ARCH_cos
+#define ARCH_exp
+#define ARCH_fabs
+#define ARCH_floor
+#define ARCH_fmod
+#define ARCH_frexp
+#define ARCH_hypot
+#define ARCH_ldexp
+#define ARCH_log
+#define ARCH_pow
+#define ARCH_sin
+#define ARCH_sqrt
+
+#endif /* MATH_I386_H */
