ae395d8749683a62a87fb447c5063257b8b3e91c
Pierre Pronchery <khorben@defora.org>
2009-02-19 21:21:10 +0000

    Working on fd_set

--- include/kernel/linux/sys/select.h
+++ include/kernel/linux/sys/select.h
@@ -1,18 +1,17 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
-/* libc is not free software; you can redistribute it and/or modify it under
- * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
- * Unported as published by the Creative Commons organization.
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
  *
- * libc is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the Creative Commons Attribution-NonCommercial-
- * ShareAlike 3.0 Unported license for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the Creative Commons Attribution-
- * NonCommercial-ShareAlike 3.0 along with libc; if not, browse to
- * http://creativecommons.org/licenses/by-nc-sa/3.0/ */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 
 
@@ -21,6 +20,13 @@
 
 
 /* types */
+# ifndef fd_set
+#  define fd_set fd_set
+typedef struct _fd_set
+{
+	long fds_bits[8];
+} fd_set;
+# endif
 # ifndef suseconds_t
 #  define suseconds_t suseconds_t
 typedef int suseconds_t;
@@ -37,5 +43,20 @@ struct timeval
 	suseconds_t tv_usec;
 };
 #endif
+
+
+/* macros */
+# define FD_CLR(fd, fdset)	\
+	((fdset)->fds_bits[(fd) / 32] &= ~(1 << ((fd) % 8)))
+# define FD_ISSET(fd, fdset)	\
+	((fdset)->fds_bits[(fd) / 32] & (1 << ((fd) % 8)))
+# define FD_SET(fd, fdset)	\
+	((fdset)->fds_bits[(fd) / 32] |= (1 << ((fd) % 8)))
+# define FD_ZERO(fdset)		\
+	memset(fdset, 0, sizeof(fd_set))
+
+
+/* constants */
+# define FD_SETSIZE	1024
 
 #endif /* !LIBC_KERNEL_LINUX_SYS_SELECT_H */
