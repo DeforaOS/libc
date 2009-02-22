5323f2053ee9f6ea3cdeb04ad1d2656bb3b34d83
Pierre Pronchery <khorben@defora.org>
2009-02-22 01:20:46 +0000

    Adding FreeBSD support

--- include/socket/kernel/freebsd/netinet/in.h
+++ include/socket/kernel/freebsd/netinet/in.h
@@ -15,18 +15,39 @@
 
 
 
-#ifndef LIBSOCKET_COMPAT_NETINET_IN_H
-# define LIBSOCKET_COMPAT_NETINET_IN_H
+#ifndef LIBSOCKET_KERNEL_FREEBSD_NETINET_IN_H
+# define LIBSOCKET_KERNEL_FREEBSD_NETINET_IN_H
 
 
-# if defined(__FreeBSD__)
-#  include "kernel/freebsd/netinet/in.h"
-# elif defined(__linux__)
-#  include "kernel/linux/netinet/in.h"
-# elif defined(__NetBSD__)
-#  include "kernel/netbsd/netinet/in.h"
-# else
-#  warning Unsupported platform
+/* types */
+# ifndef in_addr_t
+#  define in_addr_t in_addr_t
+typedef uint32_t in_addr_t;
 # endif
 
-#endif /* !LIBSOCKET_COMPAT_NETINET_IN_H */
+# ifndef in_port_t
+#  define in_port_t in_port_t
+typedef uint16_t in_port_t;
+#endif
+
+# ifndef in_addr
+#  define in_addr in_addr
+struct in_addr
+{
+	in_addr_t s_addr;
+};
+# endif
+
+# ifndef sockaddr_in
+#  define sockaddr_in sockaddr_in
+struct sockaddr_in
+{
+	unsigned char sin_len;
+	sa_family_t sin_family;
+	in_port_t sin_port;
+	struct in_addr sin_addr;
+	char _padding[8];
+};
+# endif
+
+#endif /* !LIBSOCKET_KERNEL_FREEBSD_NETINET_IN_H */
