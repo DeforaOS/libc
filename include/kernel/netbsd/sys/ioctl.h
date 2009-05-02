9cfe314f3fc8eb36b153a585be50c45aedda241a
Pierre Pronchery <khorben@defora.org>
2009-05-02 21:24:32 +0000

    Completed NetBSD-specific macros and constants set for ioctls

--- include/kernel/netbsd/sys/ioctl.h
+++ include/kernel/netbsd/sys/ioctl.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* libc is not free software; you can redistribute it and/or modify it under
  * the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
@@ -22,15 +22,20 @@
 
 /* macros */
 # define _IOC(inout, group, num, len) \
-	(inout | ((len & IOCPARM_MASK) << 16) | ((group) << 8) | (num))
-# define _IOR(g, n, t) _IOC(IOC_OUT, (g), (n), sizeof(t))
+		(inout | ((len & IOCPARM_MASK) << 16) | ((group) << 8) | (num))
+# define _IO(g, n)	_IOC(IOC_VOID, (g), (n), 0)
+# define _IOR(g, n, t)	_IOC(IOC_OUT, (g), (n), sizeof(t))
+# define _IOW(g, n, t)	_IOC(IOC_IN, (g), (n), sizeof(t))
+# define _IORW(g, n, t)	_IOC(IOC_INOUT, (g), (n), sizeof(t))
 
 
 /* constants */
 # define IOCPARM_MASK	0x1fff
+# define IOC_VOID	(unsigned long)0x20000000
 # define IOC_OUT	(unsigned long)0x40000000
+# define IOC_IN		(unsigned long)0x80000000
 # define IOC_DIRMASK	(unsigned long)0xe0000000
 
-# define TIOCGETA _IOR('t', 19, struct termios)
+# define TIOCGETA	_IOR('t', 19, struct termios)
 
 #endif /* !LIBC_KERNEL_NETBSD_SYS_IOCTL_H */
