951e293d5e3b0688cb2a21c9859955973dda333b
Pierre Pronchery <khorben@defora.org>
2009-05-25 22:41:54 +0000

    Updated copyright information

--- src/pwd.c
+++ src/pwd.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2007 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
