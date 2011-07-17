691ff21b40c9eab418a0571f7f3efe6ad9390b4c
Pierre Pronchery <khorben@defora.org>
2011-07-17 13:53:48 +0000

    Added socketpair() to FreeBSD

--- src/socket/kernel/freebsd/common.h
+++ src/socket/kernel/freebsd/common.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,5 +25,6 @@
 # define SYS_bind		104
 # define SYS_listen		106
 # define SYS_sendto		133
+# define SYS_socketpair		135
 
 #endif /* !LIBSOCKET_KERNEL_FREEBSD_COMMON_H */
