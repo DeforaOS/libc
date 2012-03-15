816ce27968d4d8eef4f5ad78636be131f0f0dda3
Pierre Pronchery <khorben@defora.org>
2012-03-15 04:57:26 +0000

    Implemented sigsetjmp() and siglongjmp() (not functional yet)

--- tests/setjmp.c
+++ tests/setjmp.c
@@ -16,9 +16,7 @@
 
 
 #include <setjmp.h>
-#ifdef DEBUG
-# include <stdio.h>
-#endif
+#include <stdio.h>
 #include <string.h>
 
 
@@ -26,17 +24,27 @@
 /* private */
 /* variables */
 static jmp_buf _jb;
+static sigjmp_buf _sjb;
 static int _ret;
 
 
 /* prototypes */
 #ifdef DEBUG
 static void _jmp_buf_dump(jmp_buf jb);
+static void _sigjmp_buf_dump(sigjmp_buf jb);
 #endif
+
+/* _setjmp */
 static void __setjmp_test1(void);
 static void __setjmp_test2(void);
+
+/* setjmp */
 static void _setjmp_test1(void);
 static void _setjmp_test2(void);
+
+/* sigsetjmp */
+static void _sigsetjmp_test1(void);
+static void _sigsetjmp_test2(void);
 
 
 /* functions */
@@ -55,9 +63,26 @@ static void _jmp_buf_dump(jmp_buf jb)
 	}
 	fputs("\n", stderr);
 }
+
+
+/* sigjmp_buf_dump */
+static void _sigjmp_buf_dump(sigjmp_buf sjb)
+{
+	size_t i;
+	char const * sep = "";
+
+	fprintf(stderr, "DEBUG: %s()\n", __func__);
+	for(i = 0; i < sizeof(sigjmp_buf) / sizeof(*sjb); i++)
+	{
+		fprintf(stderr, "%s%p", sep, sjb[i]);
+		sep = " ";
+	}
+	fputs("\n", stderr);
+}
 #endif
 
 
+/* _setjmp */
 /* _setjmp_test1 */
 static void __setjmp_test1(void)
 {
@@ -81,13 +106,14 @@ static void __setjmp_test2(void)
 }
 
 
+/* setjmp */
 /* setjmp_test1 */
 static void _setjmp_test1(void)
 {
 #ifdef DEBUG
 	fprintf(stderr, "DEBUG: %s()\n", __func__);
 #endif
-	_ret = 3;
+	_ret = 3 << 1;
 	_setjmp_test2();
 }
 
@@ -98,24 +124,46 @@ static void _setjmp_test2(void)
 #ifdef DEBUG
 	fprintf(stderr, "DEBUG: %s()\n", __func__);
 #endif
-	_ret = 4;
-	longjmp(_jb, 5);
-	_ret = 6;
+	_ret = 4 << 1;
+	longjmp(_jb, 5 << 1);
+	_ret = 6 << 1;
+}
+
+
+/* sigsetjmp */
+/* sigsetjmp_test1 */
+static void _sigsetjmp_test1(void)
+{
+#ifdef DEBUG
+	fprintf(stderr, "DEBUG: %s()\n", __func__);
+#endif
+	_ret = 3 << 2;
+	_sigsetjmp_test2();
+}
+
+
+/* sigsetjmp_test2 */
+static void _sigsetjmp_test2(void)
+{
+#ifdef DEBUG
+	fprintf(stderr, "DEBUG: %s()\n", __func__);
+#endif
+	_ret = 4 << 2;
+	siglongjmp(_sjb, 5 << 2);
+	_ret = 6 << 2;
 }
 
 
 /* public */
 /* functions */
 /* main */
-int main(void)
+int main(int argc, char * argv[])
 {
 	int res;
 
-#ifdef DEBUG
-	fprintf(stderr, "DEBUG: %s()\n", __func__);
-#endif
-	_ret = 2;
 	/* _setjmp */
+	printf("%s: %s", argv[0], "Testing _setjmp()\n");
+	_ret = 2;
 	if((res = _setjmp(_jb)) == 0)
 	{
 #ifdef DEBUG
@@ -125,9 +173,13 @@ int main(void)
 	}
 	else if(res == 5)
 		_ret = 0;
+	else
+		fprintf(stderr, "%s: %s", argv[0], "_setjmp() failed\n");
 	/* setjmp */
 	if(_ret == 0)
 	{
+		printf("%s: %s", argv[0], "Testing setjmp()\n");
+		_ret = 2;
 		memset(_jb, 0, sizeof(jmp_buf));
 		if((res = setjmp(_jb)) == 0)
 		{
@@ -136,8 +188,47 @@ int main(void)
 #endif
 			_setjmp_test1();
 		}
-		else if(res == 5)
+		else if(res == (5 << 1))
 			_ret = 0;
+		else
+			fprintf(stderr, "%s: %s", argv[0], "setjmp() failed\n");
+	}
+	/* sigsetjmp (not saving the signal mask) */
+	if(_ret == 0)
+	{
+		printf("%s: %s", argv[0], "Testing sigsetjmp(1/2)\n");
+		_ret = 2;
+		if((res = sigsetjmp(_sjb, 0)) == 0)
+		{
+#ifdef DEBUG
+			_sigjmp_buf_dump(_sjb);
+#endif
+			_sigsetjmp_test1();
+		}
+		else if(res == (5 << 2))
+			_ret = 0;
+		else
+			fprintf(stderr, "%s: %s", argv[0],
+					"sigsetjmp(1/2) failed\n");
+	}
+	/* sigsetjmp (saving the signal mask) */
+	if(_ret == 0)
+	{
+		printf("%s: %s", argv[0], "Testing sigsetjmp(2/2)\n");
+		_ret = 2;
+		memset(_sjb, 0, sizeof(sigjmp_buf));
+		if((res = sigsetjmp(_sjb, 1)) == 0)
+		{
+#ifdef DEBUG
+			_sigjmp_buf_dump(_sjb);
+#endif
+			_sigsetjmp_test1();
+		}
+		else if(res == (5 << 2))
+			_ret = 0;
+		else
+			fprintf(stderr, "%s: %s", argv[0],
+					"sigsetjmp(2/2) failed\n");
 	}
 	return _ret;
 }
