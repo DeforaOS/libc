f787dc7a4fe4c390b40db75c10ddc57ec691475e
Pierre Pronchery <khorben@defora.org>
2012-11-28 01:31:09 +0100

    Added SOMAXCONN to FreeBSD, Linux and NetBSD's <sys/socket.h>

--- include/socket/kernel/linux/sys/socket.h
+++ include/socket/kernel/linux/sys/socket.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008-2012 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,6 +39,8 @@ struct sockaddr
 # define MSG_NOSIGNAL		0x4000
 
 # define SOL_SOCKET		1
+
+# define SOMAXCONN		128
 
 # define SHUT_RD		0
 # define SHUT_WR		1
--- include/socket/kernel/netbsd/sys/socket.h
+++ include/socket/kernel/netbsd/sys/socket.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2008 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008-2012 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,6 +40,8 @@ struct sockaddr
 # define MSG_NOSIGNAL		0x0400
 
 # define SOL_SOCKET		0xffff
+
+# define SOMAXCONN		128
 
 # define SHUT_RD		0
 # define SHUT_WR		1
