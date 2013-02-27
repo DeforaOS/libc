2d30904d77251c17231d0bbd8e2eb85bb5f8b4fb
Pierre Pronchery <khorben@defora.org>
2013-02-27 03:06:19 +0100

    Implemented localeconv()

--- src/locale.c
+++ src/locale.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008-2013 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
 
 
 
+#include "limits.h"
 #include "errno.h"
 #include "locale.h"
 
@@ -23,9 +24,37 @@
 /* localeconv */
 struct lconv * localeconv(void)
 {
-	/* FIXME implement */
-	errno = ENOSYS;
-	return NULL;
+	static struct lconv ret;
+	static int set = 0;
+
+	if(set != 0)
+		return &ret;
+	set = 1;
+	ret.decimal_point = ".";
+	ret.thousands_sep = "";
+	ret.grouping = "";
+	ret.int_curr_symbol = "";
+	ret.currency_symbol = "";
+	ret.mon_decimal_point = "";
+	ret.mon_thousands_sep = "";
+	ret.mon_grouping = "";
+	ret.positive_sign = "";
+	ret.negative_sign = "";
+	ret.int_frac_digits = CHAR_MAX;
+	ret.frac_digits = CHAR_MAX;
+	ret.p_cs_precedes = CHAR_MAX;
+	ret.p_sep_by_space = CHAR_MAX;
+	ret.n_cs_precedes = CHAR_MAX;
+	ret.n_sep_by_space = CHAR_MAX;
+	ret.p_sign_posn = CHAR_MAX;
+	ret.n_sign_posn = CHAR_MAX;
+	ret.int_p_cs_precedes = CHAR_MAX;
+	ret.int_n_cs_precedes = CHAR_MAX;
+	ret.int_p_sep_by_space = CHAR_MAX;
+	ret.int_n_sep_by_space = CHAR_MAX;
+	ret.int_p_sign_posn = CHAR_MAX;
+	ret.int_n_sign_posn = CHAR_MAX;
+	return &ret;
 }
 
 
