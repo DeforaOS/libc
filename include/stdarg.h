e11c92f9bf759f05a2f29906859839217c33dac0
Pierre Pronchery <khorben@defora.org>
2013-10-24 17:49:16 +0200

    Code cleanup

--- include/stdarg.h
+++ include/stdarg.h
@@ -53,7 +53,7 @@ typedef void * va_list;
 #  define va_start(ap, arg)	__builtin_va_start((ap), (arg))
 #  define va_arg		__builtin_va_arg
 #  define va_copy		__builtin_va_copy
-#  define va_end(ap)		__builtin_va_end(ap)
+#  define va_end(ap)		__builtin_va_end((ap))
 # else /* !__i386__ */
 #  warning Unsupported architecture
 #  define va_start(ap, arg)
