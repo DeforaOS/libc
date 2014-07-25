57b85e2da24456ef128947aaf51a543417a8b02e
Pierre Pronchery <khorben@defora.org>
2014-07-25 19:46:19 +0200

    Updated the copyright line

--- include/time.h
+++ include/time.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2005-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2005-2014 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
