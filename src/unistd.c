8ea6ba4f7681cfaff419d6d5d7243f4b786ca3ec
Pierre Pronchery <khorben@defora.org>
2015-02-24 21:42:46 +0100

    Code cleanup

--- src/unistd.c
+++ src/unistd.c
@@ -102,7 +102,7 @@ extern int _brk(void * addr); /* XXX in-kernel prototype */
 
 int brk(void * addr)
 {
-	return _brk(addr) != -1 ? 0 : -1;
+	return (_brk(addr) != -1) ? 0 : -1;
 }
 #elif defined(SYS_sbrk) /* !SYS_brk && SYS_sbrk */
 int brk(void * addr)
