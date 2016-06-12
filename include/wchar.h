fadf49d72f38710f2b106ac307ef597ccaf5f1c4
Pierre Pronchery <khorben@defora.org>
2016-06-12 22:00:55 +0200

    Update the copyright notice

--- include/wchar.h
+++ include/wchar.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2008-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2008-2016 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* All rights reserved.
  *
