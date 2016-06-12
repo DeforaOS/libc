2f0793f08fdd8aa603a7ee5a78400d202d5b7a1d
Pierre Pronchery <khorben@defora.org>
2016-06-12 21:40:36 +0200

    Re-indent

--- include/stdint.h
+++ include/stdint.h
@@ -38,14 +38,14 @@ typedef short int int16_t;
 typedef int int32_t;
 typedef long long int64_t;
 typedef unsigned char uint8_t;
-#ifndef uint16_t
-# define uint16_t uint16_t
+# ifndef uint16_t
+#  define uint16_t uint16_t
 typedef unsigned short uint16_t;
-#endif
-#ifndef uint32_t
-# define uint32_t uint32_t
+# endif
+# ifndef uint32_t
+#  define uint32_t uint32_t
 typedef unsigned int uint32_t;
-#endif
+# endif
 typedef unsigned long long uint64_t;
 
 # ifndef intmax_t
