9a159ce357329131ebdf98249aa30a7d0bbbcb56
Pierre Pronchery <khorben@defora.org>
2016-06-24 18:16:42 +0200

    Add some IPv6 definitions for FreeBSD

--- include/socket/kernel/freebsd/netinet/in.h
+++ include/socket/kernel/freebsd/netinet/in.h
@@ -63,4 +63,25 @@ struct sockaddr_in
 };
 # endif
 
+# ifndef in6_addr
+#  define in6_addr in6_addr
+struct in6_addr
+{
+	uint8_t s6_addr[16];
+};
+# endif
+
+# ifndef sockaddr_in6
+#  define sockaddr_in6 sockaddr_in6
+struct sockaddr_in6
+{
+	uint8_t sin6_len;
+	sa_family_t sin6_family;
+	in_port_t sin6_port;
+	uint32_t sin6_flowinfo;
+	struct in6_addr sin6_addr;
+	uint32_t sin6_scope_id;
+};
+# endif
+
 #endif /* !LIBSOCKET_KERNEL_FREEBSD_NETINET_IN_H */
