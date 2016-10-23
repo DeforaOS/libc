f14f19adfed6412474e87b17466b6b05cca13a4e
Ali H. Fardan <raiz@firemail.cc>
2016-10-23 19:05:16 +0300

    added test for strftime %a

--- tests/time.c
+++ tests/time.c
@@ -167,6 +167,7 @@ int main(int argc, char * argv[])
 	res += _mktime(argv[0]);
 	res += _strftime(argv[0], "%H:%M:%S", "00:00:00");
 	res += _strftime(argv[0], "%b", "Jan");
+	res += _strftime(argv[0], "%a", "Sun");
 	res += _strptime(argv[0]);
 	res += _tzset(argv[0]);
 	return (res == 0) ? 0 : 2;
