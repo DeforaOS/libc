157da54c7615332acc75740caac410c40cc556a7
Pierre Pronchery <khorben@defora.org>
2016-12-20 03:30:32 +0100

    Move __stack_chk_fail_local() into a static library
    This fixes support for SSP on Linux/i386.
    The GCC specs file needs to be updated as well.

--- src/ssp/ssp.c
+++ src/ssp/ssp.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2015-2016 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2016 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS System libc */
 /* All rights reserved.
  *
@@ -28,41 +28,18 @@
 
 
 
-#include "sys/sysctl.h"
-#include "unistd.h"
-#include "stdlib.h"
-#include "syscalls.h"
 #include "ssp.h"
 
 
 #if defined(__SSP__)
-/* variables */
-/* default to the "terminator canary" */
-long __stack_chk_guard[8] = { 0, 0, '\n', 255, 0, 0, 0, 0 };
-
-
 /* functions */
-/* stack_chk_fail */
-void __stack_chk_fail(void)
+# ifdef __PIC__
+#  ifdef __i386__
+/* stack_chk_fail_local */
+void __stack_chk_fail_local(void)
 {
-	const char buf[] = "stack overflow detected: terminated\n";
-
-	/* output on the standard error stream */
-	write(2, buf, sizeof(buf) - 1);
-	/* trigger a crash */
-	abort();
-	_exit(127);
+	__stack_chk_fail();
 }
-
-
-/* stack_chk_setup */
-void __stack_chk_setup(void)
-{
-	size_t i;
-
-	if(__stack_chk_guard[0] == 0)
-		for(i = 0; i < sizeof(__stack_chk_guard)
-				/ sizeof(*__stack_chk_guard); i++)
-			__stack_chk_guard[i] = arc4random();
-}
+#  endif
+# endif
 #endif
