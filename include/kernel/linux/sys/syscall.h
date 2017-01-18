8d6ed2a2a50c7ed72f8288121dfd9f156ab6a4fc
Pierre Pronchery <khorben@defora.org>
2017-01-18 16:03:54 +0100

    Code cleanup

--- include/kernel/linux/sys/syscall.h
+++ include/kernel/linux/sys/syscall.h
@@ -34,13 +34,13 @@
 
 # ifndef __amd64__ /* amd64 is totally specific */
 #  define SYS_exit		1
-#  define SYS_fork	  	2
-#  define SYS_read	  	3
-#  define SYS_write	  	4
-#  define SYS_open	  	5
-#  define SYS_close	  	6
+#  define SYS_fork		2
+#  define SYS_read		3
+#  define SYS_write		4
+#  define SYS_open		5
+#  define SYS_close		6
 #  define SYS_creat		8
-#  define SYS_link	  	9
+#  define SYS_link		9
 #  define SYS_unlink		10
 #  define SYS_execve		11
 #  define SYS_chdir		12
