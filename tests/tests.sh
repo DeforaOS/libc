fc6773e1067e0efebef3a0cd53a9235cdbdae13a
Pierre Pronchery <khorben@defora.org>
2017-03-10 00:56:37 +0900

    Really test reproducibly
    - Use $SOURCE_DATE_EPOCH
    - Force a default timezone

--- tests/tests.sh
+++ tests/tests.sh
@@ -46,8 +46,8 @@ DATE="date"
 #date
 _date()
 {
-	if [ -n "$SOURCE_EPOCH" ]; then
-		$DATE -d "@$SOURCE_EPOCH" '+%a %b %d %T %Z %Y'
+	if [ -n "$SOURCE_DATE_EPOCH" ]; then
+		TZ=UTC $DATE -d "@$SOURCE_DATE_EPOCH" '+%a %b %d %T %Z %Y'
 	else
 		$DATE
 	fi
