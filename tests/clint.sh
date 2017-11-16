bf5846e3552dbd59f7ca0241daf0d0adf5d19edd
Pierre Pronchery <khorben@defora.org>
2017-11-16 21:32:17 +0100

    Import the "clint.sh" test
    This test is still disabled for the moment, as it does not complete yet.

---
+++ tests/clint.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+#$Id$
+#Copyright (c) 2016-2017 Pierre Pronchery <khorben@defora.org>
+#
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+#variables
+CFLAGS=
+CPPFLAGS=
+PROGNAME="clint.sh"
+PROJECTCONF="../project.conf"
+#executables
+DATE="date"
+DEBUG="_debug"
+FIND="find"
+LINT="lint -g"
+SORT="sort -n"
+TR="tr"
+
+
+#functions
+#clint
+_clint()
+{
+	ret=0
+	subdirs="data doc src tests tools"
+
+	$DATE
+	while read line; do
+		case "$line" in
+			"["*)
+				break
+				;;
+			"subdirs="*)
+				subdirs=${line#subdirs=}
+				subdirs=$(echo "$subdirs" | $TR ',' ' ')
+				;;
+		esac
+	done < "$PROJECTCONF"
+	for subdir in $subdirs; do
+		[ -d "../$subdir" ] || continue
+		for filename in $($FIND "../$subdir" -type f -a -name '*.c' | $SORT); do
+			(_clint_file "$filename")
+			if [ $? -eq 0 ]; then
+				echo "$PROGNAME: $filename: OK" 1>&2
+			else
+				echo "$PROGNAME: $filename: FAIL" 1>&2
+				ret=2
+			fi
+		done
+	done
+	return $ret
+}
+
+_clint_file()
+{
+	echo
+	$DEBUG $LINT $CPPFLAGS $CFLAGS "$filename" 2>&1
+	ret=$?
+	if [ $ret -eq 0 ]; then
+		echo "OK"
+	else
+		echo "FAIL"
+	fi
+	return $ret
+}
+
+
+#debug
+_debug()
+{
+	echo "$@" 1>&3
+	"$@"
+	res=$?
+	#ignore errors when the command is not available
+	[ $res -eq 127 ]					&& return 0
+	return $res
+}
+
+
+#usage
+_usage()
+{
+	echo "Usage: $PROGNAME [-c] target..." 1>&2
+	return 1
+}
+
+
+#warning
+_warning()
+{
+	echo "$PROGNAME: $@" 1>&2
+	return 2
+}
+
+
+#main
+clean=0
+while getopts "cO:P:" name; do
+	case "$name" in
+		c)
+			clean=1
+			;;
+		O)
+			export "${OPTARG%%=*}"="${OPTARG#*=}"
+			;;
+		P)
+			#XXX ignored for compatibility
+			;;
+		?)
+			_usage
+			exit $?
+			;;
+	esac
+done
+shift $((OPTIND - 1))
+if [ $# -lt 1 ]; then
+	_usage
+	exit $?
+fi
+
+#clean
+[ $clean -ne 0 ] && exit 0
+
+exec 3>&1
+while [ $# -gt 0 ]; do
+	target="$1"
+	shift
+
+	_clint > "$target"					|| exit 2
+done
