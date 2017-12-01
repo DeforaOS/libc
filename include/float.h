cddab9f148d99903ff83cb781324558776722798
Pierre Pronchery <khorben@defora.org>
2017-12-01 15:30:43 +0100

    Detect and define more constants
    This sets {DBL,FLT,LDBL}_{MAX,MAX_10_EXP,MAX_EXP,MIN,MIN_10_EXP,MIN_EXP}
    if they can be determined.

--- include/float.h
+++ include/float.h
@@ -33,7 +33,24 @@
 
 
 /* constants */
-# define DBL_MAX_EXP		1024
+# ifdef __DBL_MAX__
+#  define DBL_MAX		__DBL_MAX__
+# endif
+# ifdef __DBL_MAX_10_EXP__
+#  define DBL_MAX_10_EXP	__DBL_MAX_10_EXP__
+# endif
+# ifdef __DBL_MAX_EXP__
+#  define DBL_MAX_EXP		__DBL_MAX_EXP__
+# endif
+# ifdef __DBL_MIN__
+#  define DBL_MIN		__DBL_MIN__
+# endif
+# ifdef __DBL_MIN_10_EXP__
+#  define DBL_MIN_10_EXP	__DBL_MIN_10_EXP__
+# endif
+# ifdef __DBL_MIN_EXP__
+#  define DBL_MIN_EXP		__DBL_MIN_EXP__
+# endif
 
 # ifdef __FLT_EVAL_METHOD__
 #  define FLT_EVAL_METHOD	__FLT_EVAL_METHOD__
@@ -49,6 +66,42 @@
 #  define FLT_ROUNDS		-1
 # endif
 
-# define LDBL_MAX_EXP		1024
+# ifdef __FLT_MAX__
+#  define FLT_MAX		__FLT_MAX__
+# endif
+# ifdef __FLT_MAX_10_EXP__
+#  define FLT_MAX_10_EXP	__FLT_MAX_10_EXP__
+# endif
+# ifdef __FLT_MAX_EXP__
+#  define FLT_MAX_EXP		__FLT_MAX_EXP__
+# endif
+# ifdef __FLT_MIN__
+#  define FLT_MIN		__FLT_MIN__
+# endif
+# ifdef __FLT_MIN_10_EXP__
+#  define FLT_MIN_10_EXP	__FLT_MIN_10_EXP__
+# endif
+# ifdef __FLT_MIN_EXP__
+#  define FLT_MIN_EXP		__FLT_MIN_EXP__
+# endif
+
+# ifdef __LDBL_MAX__
+#  define LDBL_MAX		__LDBL_MAX__
+# endif
+# ifdef __LDBL_MAX_10_EXP__
+#  define LDBL_MAX_10_EXP	__LDBL_MAX_10_EXP__
+# endif
+# ifdef __LDBL_MAX_EXP__
+#  define LDBL_MAX_EXP		__LDBL_MAX_EXP__
+# endif
+# ifdef __LDBL_MIN__
+#  define LDBL_MIN		__LDBL_MIN__
+# endif
+# ifdef __LDBL_MIN_10_EXP__
+#  define LDBL_MIN_10_EXP	__LDBL_MIN_10_EXP__
+# endif
+# ifdef __LDBL_MIN_EXP__
+#  define LDBL_MIN_EXP		__LDBL_MIN_EXP__
+# endif
 
 #endif /* !LIBC_FLOAT_H */
