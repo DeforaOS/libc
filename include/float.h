5a31ee92d78d10f1a620008426ec6565a4ce0c81
Pierre Pronchery <khorben@defora.org>
2017-12-01 15:39:27 +0100

    Detect and define more constants

--- include/float.h
+++ include/float.h
@@ -33,6 +33,15 @@
 
 
 /* constants */
+# ifdef __DBL_DIG__
+#  define DBL_DIG		__DBL_DIG__
+# endif
+# ifdef __DBL_EPSILON__
+#  define DBL_EPSILON		__DBL_EPSILON__
+# endif
+# ifdef __DBL_MANT_DIG__
+#  define DBL_MANT_DIG		__DBL_MANT_DIG__
+# endif
 # ifdef __DBL_MAX__
 #  define DBL_MAX		__DBL_MAX__
 # endif
@@ -52,6 +61,10 @@
 #  define DBL_MIN_EXP		__DBL_MIN_EXP__
 # endif
 
+# ifdef __DECIMAL_DIG__
+#  define DECIMAL_DIG		__DECIMAL_DIG__
+# endif
+
 # ifdef __FLT_EVAL_METHOD__
 #  define FLT_EVAL_METHOD	__FLT_EVAL_METHOD__
 # else
@@ -66,6 +79,15 @@
 #  define FLT_ROUNDS		-1
 # endif
 
+# ifdef __FLT_DIG__
+#  define FLT_DIG		__FLT_DIG__
+# endif
+# ifdef __FLT_EPSILON__
+#  define FLT_EPSILON		__FLT_EPSILON__
+# endif
+# ifdef __FLT_MANT_DIG__
+#  define FLT_MANT_DIG		__FLT_MANT_DIG__
+# endif
 # ifdef __FLT_MAX__
 #  define FLT_MAX		__FLT_MAX__
 # endif
@@ -85,6 +107,15 @@
 #  define FLT_MIN_EXP		__FLT_MIN_EXP__
 # endif
 
+# ifdef __LDBL_DIG__
+#  define LDBL_DIG		__LDBL_DIG__
+# endif
+# ifdef __LDBL_EPSILON__
+#  define LDBL_EPSILON		__LDBL_EPSILON__
+# endif
+# ifdef __LDBL_MANT_DIG__
+#  define LDBL_MANT_DIG		__LDBL_MANT_DIG__
+# endif
 # ifdef __LDBL_MAX__
 #  define LDBL_MAX		__LDBL_MAX__
 # endif
