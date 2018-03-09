6e4936a7df475196a993550defcc8691025b77df
Pierre Pronchery <khorben@defora.org>
2018-03-09 02:39:06 +0100

    Add struct msghdr on OpenBSD

--- include/socket/kernel/openbsd/sys/socket.h
+++ include/socket/kernel/openbsd/sys/socket.h
@@ -33,6 +33,20 @@
 
 
 /* types */
+# ifndef msghdr
+#  define msghdr msghdr
+struct msghdr
+{
+	void * msg_name;
+	socklen_t msg_namelen;
+	struct iovec * msg_iov;
+	unsigned int msg_iovlen;
+	void * msg_control;
+	socklen_t msg_controllen;
+	int msg_flags;
+};
+# endif
+
 # ifndef sa_family_t
 #  define sa_family_t sa_family_t
 typedef unsigned char sa_family_t;
